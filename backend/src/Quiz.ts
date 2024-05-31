c6b2e6f288b7d32e023d615c31b8835a88d87a31
Ratan Gulati <ratangulati.dev@gmail.com>
2024-05-31 19:36:50 +0530

    updated quiz file

--- backend/src/Quiz.ts
+++ backend/src/Quiz.ts
@@ -1,27 +1,64 @@
 import { IoManager } from "./managers/IoManager";
 
+export type AlloweSubmissions =  0 | 1 | 2 | 3
+const PROBLEM_TIME_S = 20;
+
+
+interface User {
+    name: string;
+    id: string;
+    points: number;
+}
+
+interface Submission {
+    problemId: string;
+    userId: string;
+    isCorrect: boolean;
+    optionSelected: AlloweSubmissions
+}
+
 interface Problem {
-    title: string,
-    description : string,
-    image: string,
-    answer: string,
+    id: string;
+    title: string;
+    description : string;
+    image: string;
+    startTime?: number;
+    answer:  AlloweSubmissions;
     option: {
         id: number;
-        title: string
-    }
+        title: string;
+    }[]
+    submissions: Submission[]
 }
 
 
 export class Quiz {
-    private roomId: string;
+    public roomId: string;
     private hasStarted: boolean;
     private problems: Problem[];
-    private activeProblem = 0;
+    private activeProblem = number;
+    private users: User[];
+    private currentState: "leaderboard" | "question" | "not_started" | "ended";
+    
 
     constructor(roomId: string) {
         this.roomId = roomId;
         this.hasStarted = false;
         this.problems = []
+        this.users = [];
+        this.currentState = "not_started";
+        console.log("room created");
+        setInterval(() => {
+            this.debug();
+        }, 10000)
+    }
+    debug() {
+        console.log("----debug---")
+        console.log(this.roomId)
+        console.log(JSON.stringify(this.problems))
+        console.log(this.users)
+        console.log(this.currentState)
+        console.log(this.activeProblem);
     }
 
     addProblem(problem: Problem) {
@@ -29,23 +66,116 @@ export class Quiz {
     }
     start() {
         this.hasStarted = true;
-        const io = IoManager.getIo();
-        io.emit("CHANGE_PROBLEM", {
-            problems: this.problems[0]
+        this.setActiveProblem(this.problems[0]);
+    }
+
+    setActiveProblem(problem: Problem) {
+        console.log("set active problem")
+        this.currentState = "question"
+        problem.startTime = new Date().getTime();
+        problem.submissions = [];
+        IoManager.getIo().to(this.roomId).emit("problem", {
+            problem
+        })
+        setTimeout(() => {
+            this.sendLeaderboard();
+        }, PROBLEM_TIME_S * 1000);
+    }
+
+    sendLeaderboard() {
+        console.log("send leaderboard")
+        this.currentState = "leaderboard"
+        const leaderboard = this.getLeaderboard();
+        IoManager.getIo().to(this.roomId).emit("leaderboard", {
+            leaderboard
         })
     }
+    
     next(){
         this.activeProblem++;
         const problem = this.problems[this.activeProblem];
-        const io = IoManager.getIo();
+        // const io = IoManager.getIo();
         if (problem) {
-            io.emit("CHANGE_PROBLEM", {
-                problem
-            })
+            this.setActiveProblem(problem);
         } else {
-            io.emit("QUIZ_END", {
+            this.activeProblem--;
+            // send final results here
+            // IoManager.getIo().emit("QUIZ_END", {
+            //     problem
+            // })
+        }
+    }
+    genRandonString(length: number) {
+        var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()';
+        var charLength = chars.length;
+        var result = '';
+        for ( var i = 0; i < length; i++ ) {
+           result += chars.charAt(Math.floor(Math.random() * charLength));
+        }
+        return result;
+    }
+    addUser(name: string) {
+        const id = this.genRandonString(7);
+        this.users.push({
+            id,
+            name,
+            points: 0
+        })
+        return id
+    }
+    submit(userId: string, roomId: string, problemId: string, submission: AlloweSubmissions) {
+        const problem = this.problems.find(x => x.id == problemId);
+        const user = this.users.find(x => x.id === userId);
+ 
+        if (!problem || !user) {
+            console.log("problem or user not found")
+            return;
+        }
+        const existingSubmission = problem.submissions.find(x => x.userId === userId);
+ 
+        if (existingSubmission) {
+            console.log("existn submissions")
+            return;
+        }
+ 
+        problem.submissions.push({
+            problemId,
+            userId,
+            isCorrect: problem.answer === submission,
+            optionSelected: submission
+        });
+        user.points += (1000 - (500 * (new Date().getTime() - problem.startTime) / (PROBLEM_TIME_S * 1000)));
+
+    }
+
+    getLeaderboard() {
+        return this.users.sort((a, b) => a.points < b.points ? 1 : -1).slice(0, 20);;
+    }
+
+    getCurrentState() {
+        if (this.currentState === "not_started") {
+            return {
+                type: "not_started"
+            }
+        }
+        if (this.currentState === "ended") {
+            return {
+                type: "ended",
+                leaderboard: this.getLeaderboard()
+            }
+        }
+        if (this.currentState === "leaderboard") {
+            return {
+                type: "leaderboard",
+                leaderboard: this.getLeaderboard()
+            }
+        }
+        if (this.currentState === "question") {
+            const problem = this.problems[this.activeProblem];
+            return {
+                type: "question",
                 problem
-            })
+            }
         }
     }
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,10 @@
 import { IoManager } from './managers/IoManager';
+import { UserManager } from './managers/Usermanager';
 
 const io = IoManager.getIo();
 
 io.listen(3000);
-
-io.on('connection', (client) => {
-  client.on('event', data => {
-    const type = data.type
-  });
-  client.on('disconnect', () => { /* … */ });
+const userManager = new UserManager();
+io.on('connection', (socket) => {
+  userManager.addUser(socket);
 });
